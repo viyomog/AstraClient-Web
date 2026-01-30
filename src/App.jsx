6bea916ce7b8eed582faf3bd1548d5ed73baf20d
Viyom <viyom@astraclient.in>
2026-01-31 02:39:29 +0530

    Add Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react"
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
@@ -37,6 +38,7 @@ function App() {
         </main>
 
         <Footer />
+        <Analytics />
       </div>
     </Router>
   );
